7f865367e769d67be9195349f81ee05d01e6879b
polinaprinii <x21137757@student.ncirl.ie>
2022-04-04 20:09:53 +0100

    Update Pre_Processing.py
    Outlining deliverables for the Pre-Processing section.

---
+++ Pre_Processing.py
@@ -0,0 +1,6 @@
+"""
+This file deals with the data selection aspect of the project in prep for machine learning techniques.
+Here we will cover multiple check to ensure the data has no missing values and if so address this issue.
+Followed by that we will ensure that outliers are reduced to a minimum.
+Finally, we will apply Dimensionality Reduction to allow for better performance due to the large size of the dataset.
+"""
