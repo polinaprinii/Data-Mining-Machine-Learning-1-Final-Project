15c1a65ef4b4838afe58141ad2c5ede0bafb1bdb
polinaprinii <x21137757@student.ncirl.ie>
2022-04-05 19:33:44 +0100

    Update Pre_Processing.py
    Introduced a visualisation aspect.

--- Pre_Processing.py
+++ Pre_Processing.py
@@ -8,15 +8,29 @@ Finally, we will apply Dimensionality Reduction to allow for better performance 
 # Importing all needed libraries:
 import pandas as pd
 import numpy as np
-
+import missingno as msno
+from matplotlib import pyplot as plt
 # Importing our file:
 
 df = pd.read_csv("/Users/polinaprinii/Desktop/Project Datasets/Flight Delays for 2019 for the USA/Flight_Weather.csv")
 print(df.head(5), "\n", df.shape, "\n")
 
+# Matrix to visualise all missing values.
+msno.bar(df)
+msno.matrix(df)
+msno.heatmap(df)
+plt.show()
+
+"""
+We can see a number of missing values are present within the datasets, though not many.
+Manly present within the actual departure and actual arrival columns.
+"""
+
 # Pandas can detect "standard missing values" such as N/A or blank cells.
 # As we are working with a considerably large amount of data, we will count the number of missing values.
 print("Below are the number of missing values within each column present: ", "\n", "\n", df.isnull().sum(), "\n")
+
+
 
 
 
