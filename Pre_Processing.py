0c63c91ec5ec87555c520f521b0f9ad1e9d820d3
polinaprinii <x21137757@student.ncirl.ie>
2022-04-06 21:06:43 +0100

    Update Pre_Processing.py

--- Pre_Processing.py
+++ Pre_Processing.py
@@ -10,7 +10,10 @@ import pandas as pd
 import numpy as np
 import missingno as msno
 from matplotlib import pyplot as plt
-import exrex
+import random
+from random import randrange
+from datetime import timedelta
+from datetime import datetime
 
 # Importing our file:
 
@@ -33,11 +36,37 @@ Manly present within the actual departure and actual arrival columns.
 print("Below are the number of missing values within each column present: ", "\n", "\n", df.isnull().sum(), "\n")
 
 # Start filling and or replacing the null/missing values.
-df['tail_number'] = df['tail_number'].fillna(exrex.getone(r'(\d "N" + {1,4}){4}'))
+"""
+ We first look at the tail number column.
+ Using the random functionality in Python we structure a tail number.
+"""
+n1 = "%03d" % random.randint(0, 999)
+n2 = random.randint(65, 90)
+n3 = random.randint(65, 90)
+tailnumber = "N" + n1 + chr(n2) + chr(n3)
+print(tailnumber)
+
+# # Applying the change to the tail number column.
+df['tail_number'] = df['tail_number'].fillna(tailnumber)
 
 
+# Next we move to addressing the missing values within the actual departure and arrival columns.
+# First we build a function which will create a random date-time value.
+def random_date(start, end):
+    """
+    This function will return a random datetime between two datetime
+    objects.
+    """
+    delta = end - start
+    int_delta = (delta.days * 24 * 60)
+    random_second = randrange(int_delta)
+    return start + timedelta(seconds=random_second)
 
+# Set start and end date, for simplicity purposes we will generate dates for the month of December.
+d1 = datetime.strptime('1/12/2019 00:00', '%d/%m/%Y %H:%M')
+d2 = datetime.strptime('31/12/2019 23:55', '%d/%m/%Y %H:%M')
 
+# Fill all missing values within the actual departure date time column.
+df['actual_departure_dt'] = df['actual_departure_dt'].fillna(random_date(d1, d2).strftime("%Y-%m-%d %H:%M"))
 
-
-
+print("Below are the number of missing values within each column present: ", "\n", "\n", df.isnull().sum(), "\n")
