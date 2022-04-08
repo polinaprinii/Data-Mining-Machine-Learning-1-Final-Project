d278f57ee1a152cba6fc54635bac05dceb2e84f7
polinaprinii <x21137757@student.ncirl.ie>
2022-04-08 17:32:03 +0100

    Update Transformation.py

--- Transformation.py
+++ Transformation.py
@@ -3,6 +3,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.feature_selection import mutual_info_classif
+
 df = pd.read_csv("/Users/polinaprinii/Desktop/Project Datasets/Flight Delays for 2019 for the USA/Cleansed_Flight_Weather.csv")
 
 print(df.shape)
@@ -12,3 +14,11 @@ cor = df.corr()
 plt.figure(figsize = (20, 10))
 sns.heatmap(cor, annot = True)
 plt.show()
+
+array = df.values
+X = array[:,0:34]
+Y = array[:,34]
+
+importance = mutual_info_classif(X,Y)
+feat_importance = pd.Series(importance, df.columns[0: len(df.columns)-1])
+feat_importance.plot(kind='barh', color='teal')
