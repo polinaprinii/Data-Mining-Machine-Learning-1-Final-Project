aecbcb4bc72d186279e1e1aa2f6e178fcfb4dfe2
polinaprinii <x21137757@student.ncirl.ie>
2022-04-23 00:50:52 +0100

    Update Data_Mining_KMeans_Clusters.py

--- Data_Mining_KMeans_Clusters.py
+++ Data_Mining_KMeans_Clusters.py
@@ -9,10 +9,18 @@ import numpy as np
 
 # Importing our two datasets into numpy arrays:
 # np1 - Unstandardised dataset.
-np1 = np.genfromtxt("/Users/polinaprinii/Desktop/Project Datasets/Flight Delays for 2019 for the USA/Selection_pre_PCA.csv")
+np1 = np.genfromtxt(
+    "/Users/polinaprinii/Desktop/Project Datasets/Flight Delays for 2019 for the USA/Selection_pre_PCA.csv")
 
+print(np1)
 # Standardised and reduced dataset.
-np2 = np.genfromtxt("/Users/polinaprinii/Desktop/Project Datasets/Flight Delays for 2019 for the USA/Selection_PCA.csv")
+#np2 = np.genfromtxt("/Users/polinaprinii/Desktop/Project Datasets/Flight Delays for 2019 for the USA/Selection_PCA.csv")
 
-
+# # Standardising data using StandardScaler()
+# features_np1 = np1
+#
+# scaler = StandardScaler()
+# scaled_features_np1 = scaler.fit_transform(features_np1)
+#
+# print(scaled_features_np1[:5])
 
