44aaeb5dd323eb7823f52fdbf509f9b80d7f9c5c
polinaprinii <x21137757@student.ncirl.ie>
2022-04-22 23:41:38 +0100

    Update Transformation.py
    Applied standardisation, Principle Component Analysis, a Linear Dimensionality Reduction technique.
    Extracted two dataframes.
    df 1: Pre PCA application
    df2: PCA application

--- Transformation.py
+++ Transformation.py
@@ -78,20 +78,88 @@ selection = ['departure_delay', 'arrival_delay', 'delay_carrier', 'delay_weather
              'delay_late_aircarft_arrival', 'HourlyDryBulbTemperature_x', 'HourlyPrecipitation_x', 'HourlyVisibility_x',
              'HourlyDryBulbTemperature_y', 'HourlyPrecipitation_y', 'HourlyVisibility_y']
 
-# Setting selection to a new dataframe which will be exported before and after PCA dimensionality reduction.
-new_df = df[selection]
+# Filtering out the target column - Origin Airport
+target = ['origin_airport']
+
+# Assigning the selection to a new dataframe which will be standardised, and dimensionality reduction applied.
+selection_df = df[selection]
+
+# Now we proceed to standardising the feature selection.
+scaled_features = df[selection]
+print(scaled_features.head(5))
+scaled_features = StandardScaler().fit_transform(scaled_features)
+
+""" 
+The Standard Scaler transforms the df into a numpy array after standardised. 
+Due to this we re-format the numpy array back into a dataframe as PCA cannot be applied to an array. 
+"""
+scaled_features_df = pd.DataFrame(scaled_features, index=df[selection].index, columns=df[selection].columns)
+
+# Sanity check to see the re-format was successful.
+print(scaled_features_df.head(5), "\n")
+print(scaled_features_df.shape)
+
+# Now we move to reducing the dimension of our standardised dataframe from 12 columns to 6 columns.
+pca = PCA(n_components=6) # setting the limit of reduction.
+principalComponents = pca.fit_transform(scaled_features_df)
+print(principalComponents)
+
+principalDf = pd.DataFrame(data = principalComponents
+             , columns = ['principal component 1', 'principal component 2',
+                          'principal component 3', 'principal component 4',
+                          'principal component 5', 'principal component 6'])
+print(principalDf.head(5))
+print(principalDf.shape)
+
+# Lastly we concatenate the reduced dataset with a single column from the original source which specifies the origin airport.
+finalDf = pd.concat([principalDf, df[target]], axis = 1)
+print(finalDf.head(5))
+
+# Visualise results.
+fig = plt.figure(figsize = (8,8))
+ax = fig.add_subplot(1,1,1)
+ax.set_xlabel('Principal Component 1', fontsize = 15)
+ax.set_ylabel('Principal Component 2', fontsize = 15)
+ax.set_title('2 component PCA', fontsize = 20)
+targets = ['JFK', 'LAX', 'MIA']
+colors = ['r', 'g', 'b']
+for target, color in zip(targets,colors):
+    indicesToKeep = finalDf['origin_airport'] == target
+    ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1']
+               , finalDf.loc[indicesToKeep, 'principal component 2']
+               , c = color
+               , s = 50)
+ax.legend(targets)
+ax.grid()
+plt.show()
+
+# Next we move to extract 2 files.
+# First we filter our second selection.
+final_selection = ['origin_airport','departure_delay', 'arrival_delay', 'delay_carrier', 'delay_weather','delay_national_aviation_system',
+                    'delay_late_aircarft_arrival', 'HourlyDryBulbTemperature_x', 'HourlyPrecipitation_x', 'HourlyVisibility_x',
+                    'HourlyDryBulbTemperature_y', 'HourlyPrecipitation_y', 'HourlyVisibility_y']
+
+extract_df = df[final_selection]
 
 # Extracting selection prior to PCA application as we will look to apply K-Means clustering to both pre PCA and after PCA.
 def export():
 # Restrict file from duplicating.
     if os.path.exists(
-        "/Users/polinaprinii/Desktop/Project Datasets/Flight Delays for 2019 for the USA/Flight_Weather.csv"):
+        "/Users/polinaprinii/Desktop/Project Datasets/Flight Delays for 2019 for the USA/Selection_pre_PCA.csv"):
+        pass
+
+    if os.path.exists(
+        "/Users/polinaprinii/Desktop/Project Datasets/Flight Delays for 2019 for the USA/Selection_PCA.csv"):
         pass
 
     else:
 # Export to csv
-        new_df.to_csv(
-            "/Users/polinaprinii/Desktop/Project Datasets/Flight Delays for 2019 for the USA/Selection.csv",
+        extract_df.to_csv(
+            "/Users/polinaprinii/Desktop/Project Datasets/Flight Delays for 2019 for the USA/Selection_pre_PCA.csv",
+            index=False, encoding='utf-8-sig')
+
+        finalDf.to_csv(
+            "/Users/polinaprinii/Desktop/Project Datasets/Flight Delays for 2019 for the USA/Selection_PCA.csv",
             index=False, encoding='utf-8-sig')
 
 export() # Due to laptop constrictions we will look to import the exported file following the pre-processing step.
