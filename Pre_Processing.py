4dc9d3b8a079d45e4a446d1f4b40057e74dcacbd
polinaprinii <x21137757@student.ncirl.ie>
2022-04-24 13:47:28 +0100

    Update Pre_Processing.py
    Comment added

--- Pre_Processing.py
+++ Pre_Processing.py
@@ -1,5 +1,6 @@
 """
-This file deals with the pre-processing aspect of the project in prep for machine learning techniques.
+This file deals with step two of the KDD methodology, the pre-processing aspect of the project in prep for
+machine learning techniques.
 Here we will cover multiple check to ensure the data has no missing values and if so address this issue.
 Followed by that we will ensure that outliers are reduced to a minimum.
 Finally, we will apply Dimensionality Reduction to allow for better performance due to the large size of the dataset.
