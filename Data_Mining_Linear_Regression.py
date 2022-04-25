2292bfd4ae2723d1c99825b393918b40fc171cc9
polinaprinii <x21137757@student.ncirl.ie>
2022-04-25 19:29:10 +0100

    Update Data_Mining_Linear_Regression.py
    Finalised Linear Regression

--- Data_Mining_Linear_Regression.py
+++ Data_Mining_Linear_Regression.py
@@ -12,6 +12,7 @@ from sklearn.metrics import mean_absolute_percentage_error
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error
 import pandas as pd
+import numpy as np
 
 # For purposes of exploration we import both datasets, pre PCA and after PCA application.
 # Dataframe 1: Pre-PCA
@@ -25,34 +26,34 @@ plt.legend(bbox_to_anchor=(1,1), loc="upper left")
 plt.show()
 
 # Dataframe 1 Statistics:
-# print(df_prePCA.info, "\n")
-# print(df_prePCA.describe(), "\n")
-# print(df_prePCA.head(), "\n")
-# print(list(df_prePCA.columns.values), '\n')
+print(df_prePCA.info, "\n")
+print(df_prePCA.describe(), "\n")
+print(df_prePCA.head(), "\n")
+print(list(df_prePCA.columns.values), '\n')
 
-# # Identifying linear relationships between our target variable and feature variables within Dataframe 1.
-# plt.scatter(df_prePCA['departure_delay'], df_prePCA['arrival_delay'])
-# plt.show()
-# plt.scatter(df_prePCA['departure_delay'], df_prePCA['delay_carrier'])
-# plt.show()
-# plt.scatter(df_prePCA['departure_delay'], df_prePCA['delay_carrier'])
-# plt.show()
-# plt.scatter(df_prePCA['departure_delay'], df_prePCA['delay_national_aviation_system'])
-# plt.show()
-# plt.scatter(df_prePCA['departure_delay'], df_prePCA['delay_late_aircarft_arrival'])
-# plt.show()
-# plt.scatter(df_prePCA['departure_delay'], df_prePCA['HourlyDryBulbTemperature_x'])
-# plt.show()
-# plt.scatter(df_prePCA['departure_delay'], df_prePCA['HourlyPrecipitation_x'])
-# plt.show()
-# plt.scatter(df_prePCA['departure_delay'], df_prePCA['HourlyVisibility_x'])
-# plt.show()
-# plt.scatter(df_prePCA['departure_delay'], df_prePCA['HourlyDryBulbTemperature_y'])
-# plt.show()
-# plt.scatter(df_prePCA['departure_delay'], df_prePCA['HourlyPrecipitation_y'])
-# plt.show()
-# plt.scatter(df_prePCA['departure_delay'], df_prePCA['HourlyVisibility_y'])
-# plt.show()
+# Identifying linear relationships between our target variable and feature variables within Dataframe 1.
+plt.scatter(df_prePCA['departure_delay'], df_prePCA['arrival_delay'])
+plt.show()
+plt.scatter(df_prePCA['departure_delay'], df_prePCA['delay_carrier'])
+plt.show()
+plt.scatter(df_prePCA['departure_delay'], df_prePCA['delay_carrier'])
+plt.show()
+plt.scatter(df_prePCA['departure_delay'], df_prePCA['delay_national_aviation_system'])
+plt.show()
+plt.scatter(df_prePCA['departure_delay'], df_prePCA['delay_late_aircarft_arrival'])
+plt.show()
+plt.scatter(df_prePCA['departure_delay'], df_prePCA['HourlyDryBulbTemperature_x'])
+plt.show()
+plt.scatter(df_prePCA['departure_delay'], df_prePCA['HourlyPrecipitation_x'])
+plt.show()
+plt.scatter(df_prePCA['departure_delay'], df_prePCA['HourlyVisibility_x'])
+plt.show()
+plt.scatter(df_prePCA['departure_delay'], df_prePCA['HourlyDryBulbTemperature_y'])
+plt.show()
+plt.scatter(df_prePCA['departure_delay'], df_prePCA['HourlyPrecipitation_y'])
+plt.show()
+plt.scatter(df_prePCA['departure_delay'], df_prePCA['HourlyVisibility_y'])
+plt.show()
 
 # Next we move to separating our features from our target for Dataframe 1.
 X = df_prePCA[['arrival_delay','delay_carrier', 'delay_weather', 'delay_national_aviation_system',
@@ -77,19 +78,19 @@ print_model = model.summary()
 
 # Interpreting the results for Dataframe 1:
 print('Summary of Results - Dataframe 1 - Pre PCA: \n',print_model, '\n')
-print('R-squared: ', round(regr.score(X_test, Y_test), 2), '\n')
-print("Mean squared error: %.3f"
+print('The R-squared value for Dataframe 1 is : ', round(regr.score(X_test, Y_test), 2), '\n')
+print("The Mean squared error for Dataframe 1 is : %.3f"
       % mean_squared_error(Y_test, Y_pred), '\n')
-print('Root-mean-square deviation: %.3f'
+print('The Root-mean-square deviation for Dataframe 1 is : %.3f'
       % mean_squared_error(Y_test, Y_pred, squared=False), '\n')
-print('Mean-absolute-percentage-error: %.3f'
+print('The Mean-absolute-percentage-error for Dataframe 1 : %.3f'
       % mean_absolute_percentage_error(Y_test, Y_pred), '\n')
 
-
+# As there are not much good statistics from Dataframe 2 we comment out the code but keep it for educational purposes.
 # Dataframe 2: PCA
-df_PCA = pd.read_csv(
-      "/Users/polinaprinii/Desktop/Project Datasets/Flight Delays for 2019 for the USA/Selection_PCA.csv"
-)
+# df_PCA = pd.read_csv(
+#       "/Users/polinaprinii/Desktop/Project Datasets/Flight Delays for 2019 for the USA/Selection_PCA.csv"
+# )
 # # Visualising Dataframe 2.
 # df_PCA.plot(figsize=(25,5))
 # plt.title('Visualising df2 after PCA application')
@@ -106,31 +107,33 @@ df_PCA = pd.read_csv(
 # plt.scatter(df_PCA['principal component 1'], df_PCA['principal component 2'])
 # plt.show()
 
-# Next we move to separating our features from our target for Dataframe 1.
-k = df_PCA[['principal component 1']]
+# Assign features and targets:
+# x = np.array(df_PCA['principal component 1']).reshape(-1, 1)
+# y = np.array(df_PCA['principal component 2']).reshape(-1, 1)
 
-n = df_PCA['principal component 2']
+# Dropping any NA values.
+# df_PCA.dropna(inplace = True)
 
 # Secondly we split the data into 30% test and 70% train for Dataframe 2:
-k_train, k_test, n_train, n_test = train_test_split(k, n, test_size=0.3)
+# x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
 
 # Apply Multiple Linear Regression Analysis for Dataframe 2:
-regression = linear_model.LinearRegression()
-regression.fit(k_train, n_train)
+# regression = linear_model.LinearRegression()
+# regression.fit(x_train, y_train)
 
 # Predicting our dependent variable for Dataframe 2.
-n_pred = regression.predict(k_test)
+# y_pred = regression.predict(x_test)
 
 # Applying Ordinary Least Squared Regression to Dataframe 2.
-model = sm.OLS(n_test, k_test).fit()
-print_model = model.summary()
+# model = sm.OLS(y_test, x_test).fit()
+# print_model = model.summary()
 
 # Interpreting the results for Dataframe 1:
-print('Summary of Results - Dataframe 2 - PCA = 2: \n',print_model, '\n')
-print('R-squared: ', round(regr.score(k_test, n_test), 2), '\n')
-print("Mean squared error: %.3f"
-      % mean_squared_error(n_test, n_pred), '\n')
-print('Root-mean-square deviation: %.3f'
-      % mean_squared_error(n_test, n_pred, squared=False), '\n')
-print('Mean-absolute-percentage-error: %.3f'
-      % mean_absolute_percentage_error(n_test, n_pred), '\n')
+# print('Summary of Results - Dataframe 2 - PCA = 2: \n',print_model, '\n')
+# print('The R-squared for Dataframe 2 is : ', round(regression.score(x_test, y_test), 2), '\n')
+# print("The Mean squared error for Dataframe 2 is : %.3f"
+#       % mean_squared_error(y_test, y_pred), '\n')
+# print('The Root-mean-square deviation for Dataframe 2 is : %.3f'
+#       % mean_squared_error(y_test, y_pred, squared=False), '\n')
+# print('The Mean-absolute-percentage-error for Dataframe 2 is : %.3f'
+#       % mean_absolute_percentage_error(y_test, y_pred), '\n')
