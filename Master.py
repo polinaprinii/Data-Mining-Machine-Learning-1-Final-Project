9f542e19aaefa8722b1095114b3eb2e27320258a
polinaprinii <x21137757@student.ncirl.ie>
2022-04-28 17:37:39 +0100

    Master file

---
+++ Master.py
@@ -0,0 +1,10 @@
+# Importing all files into one Master.py file to allows for seamless running of the code in one go.
+
+import Data_Selection
+import Pre_Processing
+import Transformation
+import Data_Mining_KMeans_Clusters
+import Data_Mining_Linear_Regression
+import Data_Mining_KNN_Regression
+import Data_Mining_Random_Forest_Walk
+import Data_Mining_Enesemle_Method_Boosting
