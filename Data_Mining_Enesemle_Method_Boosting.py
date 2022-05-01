dd654a714a08ab3294c74df97769f0dbb4b7fe38
polinaprinii <x21137757@student.ncirl.ie>
2022-05-01 15:31:13 +0100

    Update Data_Mining_Enesemle_Method_Boosting.py

--- Data_Mining_Enesemle_Method_Boosting.py
+++ Data_Mining_Enesemle_Method_Boosting.py
@@ -10,6 +10,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from sklearn.ensemble import GradientBoostingRegressor, AdaBoostRegressor
 from sklearn.preprocessing import StandardScaler
+import math
+from sklearn.metrics import mean_absolute_percentage_error
 
 # Loading data:
 df = pd.read_csv('/Users/polinaprinii/Desktop/Project Datasets/Flight Delays for 2019 for the USA/Selection_pre_PCA.csv')
@@ -34,9 +36,15 @@ model = AdaBoostRegressor()
 model.fit(X_train, y_train)
 # make predictions
 y_pred = model.predict(X_test)
+rmse = math.sqrt(mean_squared_error(y_test, y_pred))
 print('Prediction:', y_pred, '\n')
-
 print('AdaBoosting Score: ', model.score(X_test, y_test), '\n')
+print('The mean-squared error of the AdaBoosting ensemble learning : %.3f'
+      % mean_squared_error(y_test, y_pred), '\n')
+print('The Root-mean-square deviation of the AdaBoosting ensemble learning : %.3f'
+      % rmse, '\n')
+print('The Mean-absolute-percentage-error of the AdaBoosting ensemble learning : %.3f'
+      % mean_absolute_percentage_error(y_test, y_pred), '\n')
 
 # initializing the boosting module with default parameters
 model_2 = GradientBoostingRegressor()
@@ -47,6 +55,8 @@ model_2.fit(X_train, y_train)
 # predicting the output on the test dataset
 pred_final = model_2.predict(X_test)
 print('GradientBoosting Score: ', model_2.score(X_test, y_test), '\n')
+print('The mean-squared error of the GradientBoosting ensemble learning : %.3f'
+      % mean_squared_error(y_test, y_pred), '\n' )
 
 
 
